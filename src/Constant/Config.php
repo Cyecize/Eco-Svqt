8bdbdd1bad5d1c2b24e2cc423ee65abf0dcc463f
Tsvetelin Yakimov <ceci2205@abv.bg>
2019-09-21 15:47:26 +0300

    Added page and pageable util classes.

--- src/Constant/Config.php
+++ src/Constant/Config.php
@@ -14,4 +14,8 @@ class Config
     public const COOKIE_NEUTRAL_LANG = "neutral";
 
     public const COOKIE_CURRENCY_NAME = "currency";
+
+    public const PAGE_SIZE_PARAM_NAME = "size";
+
+    public const PAGE_NUMBER_PARAM_NAME = "page";
 }
