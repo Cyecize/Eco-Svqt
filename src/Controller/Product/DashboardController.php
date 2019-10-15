e9bb138abd8cb3e3db6c94a19b7efaf05a32f0e4
cyecize <ceci2205@abv.bg>
2019-10-15 15:32:28 +0300

    Added dashboard controller with dummy view.

---
+++ src/Controller/Product/DashboardController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Controller\Product;
+
+use App\Controller\BaseController;
+use App\Service\Lang\LocalLanguage;
+use App\Service\Product\ProductService;
+use App\Util\Pageable;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * Class DashboardController
+ * @package App\Controller\Product
+ *
+ * @Route(path="/dashboard")
+ * @Security("has_role('ROLE_ADMIN')")
+ */
+class DashboardController extends BaseController
+{
+    /**
+     * @var ProductService
+     */
+    private $productService;
+
+    public function __construct(LocalLanguage $language, ProductService $productService)
+    {
+        parent::__construct($language);
+        $this->productService = $productService;
+    }
+
+    /**
+     * @Route(path="/", name="dashboard_get_action", methods={"GET"})
+     * @param Request $request
+     * @return Response
+     */
+    public function dashboardAction(Request $request)
+    {
+        $q = $request->get('q') || '';
+
+        return $this->render('products/dashboard/dashboard.twig', [
+            'page' => $this->productService->searchProduct($q, new Pageable($request))
+        ]);
+    }
+}
